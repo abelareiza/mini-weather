63728b96629a351891a40fe900972520ad1ce2ea
abel <abelareiza@gmail.com>
2021-02-16 14:30:48 -0500

    Import assets, add Header and Footer

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -23,11 +23,14 @@ function App() {
 
   return (
     <div className={(typeof weather.main == 'undefined') ? 'App' : (weather.main.temp >= 16) ? 'App warm' : 'App cold'}>
+      <div className='header'>
+        <div className='header-logo'>mini weather</div>
+      </div>
       <div className='search-box'>
         <input
           type='text'
           className='search-bar'
-          placeholder='Location name'
+          placeholder='Location'
           onChange={e => setQuery(e.target.value)}
           value={query}
           onKeyPress={search}
@@ -36,12 +39,13 @@ function App() {
       {(typeof weather.main == 'undefined') ? ('') : (
         <div className='results-box'>
           <div className='location-box'>
-            <div className='location'>{weather.name}, {weather.sys.country}</div>
             <div className='date'>{date.toDateString()}</div>
+            <div className='location'>{weather.name}, {weather.sys.country}</div>
           </div>
           <div className='temp-box'>
-            <div className='temp'>{weather.main.temp.toFixed(1)}°</div>
-            <div className='feels-like'>Feels like: {weather.main.feels_like.toFixed(1)}°</div>
+            <div className='temp-subtitle'>Currently</div>
+            <div className='temp'>{weather.main.temp.toFixed(1)}°c</div>
+            <div className='feels-like'>Feels like {weather.main.feels_like.toFixed(1)}°</div>
           </div>
           <div className='weather-box'>
             <div className='weather'>{weather.weather[0].main}</div>
@@ -49,6 +53,15 @@ function App() {
           </div>
         </div>
       )}
+      <div className='footer'>
+        <div className='openweather-logo'></div>
+        <div className='author'>Developed by Abel Areiza</div>
+        <div className='social-media'>
+          <div className='linkedin'><span></span>/abel-areiza</div>
+          <div className='github'><span></span>/abelareiza</div>
+          <div className='twitter'><span></span>@Enjuavel</div>
+        </div>
+      </div>
     </div>
   );
 }
